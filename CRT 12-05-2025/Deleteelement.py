c5bf619c54d63185f4cd8531d3b0131743965a39
UPPARAPALLI SURYA SASIKANTH <ussk2284@gmail.com>
2025-05-12 20:27:38 +0530

    Create Deleteelement.py

---
+++ CRT 12-05-2025/Deleteelement.py
@@ -0,0 +1,23 @@
+'''Delete an element
+Write a program to delete an element from the
+given location in an array. If index is out of
+range then print "Invalid Input"
+
+Input format:
+Read size of the arrayRead elements of the
+arrayEnter location to delete an element in the array
+
+Output format:
+Print elements of array after delete the element'''
+
+n=int(input())
+1=[]
+for _ in range(n):
+    l.append(int(input()))
+ind=int(input())
+if ind‹1 or ind>n:
+   print("Invalid Input")
+else:
+    del 1[ind-1]
+for i in 1:
+print(i)
