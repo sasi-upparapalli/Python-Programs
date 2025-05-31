a3c22829fe72edf4e9ea98d81ab128b1513421da
UPPARAPALLI SURYA SASIKANTH <ussk2284@gmail.com>
2025-05-31 20:44:22 +0530

    Create CountSEP.py

---
+++ Strings/CountSEP.py
@@ -0,0 +1,23 @@
+s=input()
+u=0
+l=0
+d=0
+c=0
+for i in s:
+   if i.isupper():
+       u=u+1
+    elif i.islower():
+       l=l+1
+    elif i.isdigit():
+       d=d+1
+    else:
+       c=c+1
+print("Upper",u)
+print("Lower",l)
+print("Digits",d)
+print("Special Characters",c)
+'''SASi ka28 @#
+Upper 3
+Lower 3
+Digits 2
+Characters 4'''
