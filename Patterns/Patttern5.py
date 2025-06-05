4855ac4bdf724cf81f8e1c48b938dde0f6bef68d
UPPARAPALLI SURYA SASIKANTH <ussk2284@gmail.com>
2025-06-05 22:32:46 +0530

    Create Patttern5.py

---
+++ Patterns/Patttern5.py
@@ -0,0 +1,25 @@
+n=int(input())
+for i in range(n):
+ for j in range(n-i):
+ print(" ",end="")
+ for j in range(i):
+ print("*",end=" ")
+ print()
+for i in range(n):
+ for j in range(i):
+ print(" ",end="")
+ for j in range(n-i):
+ print("*",end=" ")
+ print()
+ 5
+ '''
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+'''
