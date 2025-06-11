6d33ae72d8722fddf3b3f59497fad71cfffb8063
UPPARAPALLI SURYA SASIKANTH <ussk2284@gmail.com>
2025-06-11 21:57:38 +0530

    Create BubbleSort.py

---
+++ Sorts/BubbleSort.py
@@ -0,0 +1,10 @@
+ x=list(map(int,input().split()))
+ for i in range(len(x)):
+    for j in range(i+1,len(x)):
+        if x[i]>x[j]:
+            x[i],x[j]=x[j],x[i]
+ print(x)
+'''
+ 4 5 5 1 7 3 9 
+[1, 3, 4, 5, 5, 7, 9]
+'''
