a7b5aa76504d57895e1ae4a01762ba3d1236f319
UPPARAPALLI SURYA SASIKANTH <ussk2284@gmail.com>
2025-06-16 21:53:13 +0530

    Create PreDefineSort.py

---
+++ Sorts/PreDefineSort.py
@@ -0,0 +1,10 @@
+l=list(map(int,input().split()))
+l.sort()
+print("Accending order:-",*l)
+l.reverse()
+print("Decending order:-",*l)
+'''
+5 1 2
+Accending order:- 1 2 5
+Decending order:- 5 2 1
+'''
